b72d62513650e76098befc4f19957b490f67a62c
RajeevPatel <rajeev.patel@areteminds.com>
2025-07-25 21:04:59 +0530

    deployment done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My",
+  title: "Rajeev Portfolio",
   description: "Created By Rajeev Patel",
   generator: "Rajeev Patel",
 };

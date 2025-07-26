de3672b4dbf5e636da5e86b3a64b5d2eeeb639c6
rajeevpatel8985@gmail.com <rajeev.patel@areteminds.com>
2025-07-26 15:46:39 +0530

    little

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Mail } from "lucide-react";
-
+////
 export function Navbar() {
   return (
     <motion.nav
